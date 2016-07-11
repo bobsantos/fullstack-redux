67388e58ec5b40ff531f5b3b6b2b67ae06e57d1c
Bob Santos <bob.santos@tappcommerce.com>
2016-07-11 11:52:23 +0800

    voting client

--- voting-server/src/index.js
+++ voting-server/src/index.js
@@ -1,3 +1,12 @@
 import makeStore from './store';
+import startServer from './server';
 
 export const store = makeStore();
+startServer(store);
+
+store.dispatch({
+	type: 'SET_ENTRIES',
+	entries: require('./data/entries.json')
+});
+
+store.dispatch({type: 'NEXT'});
